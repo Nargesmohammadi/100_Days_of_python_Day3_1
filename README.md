100bc05fcbc647100faca2315f0ca269ff0848c6
Narges Mohammadi <Nargesmohammadii@porotonmail.com>
2022-04-16 11:42:27 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Day 3
+- This is the first part of Day-3 of Udemy's 100 days python course.
+- In this project we want to specify whether a number is even or odd.
---
+++ main.py
@@ -0,0 +1,6 @@
+number = input("enter a number please:")
+int_number = int(number)
+if int_number % 2 == 0:
+    print("this number is an Even number")
+else:
+    print("this number is an odd number")
